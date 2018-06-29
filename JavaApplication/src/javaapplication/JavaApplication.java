2c354213b13e1f18339180367b8980ab90f5ff69
Kuba <Kuba@Kuba-Komputer>
2018-06-30 00:34:18 +0200

    My super code :)

---
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication;
+
+/**
+ *
+ * @author Kuba
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        while(true){
+            System.out.println("Jest okej");
+            Thread.sleep(5*1000);
+        }
+    }
+    
+}
